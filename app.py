c0807ddd7c704f906a1d3dc0ed8e41b07ccf6bca
paulyoo97 <98494290+paulyoo97@users.noreply.github.com>
2024-01-24 22:41:03 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+from PIL import Image
+
+st.set_page_config(page_title='In The Depths', page_icon='', layout='centered')    # layout by default is center.
+
+
+st.markdown(
+    '''
+    <ul style='text-align: center;'>
+    <h1 color: black;'>In the Depths</h1>
+    <h6><em>A collection of reflections \U0001f30a</em></h6>
+    </ul>
+    ''',
+    unsafe_allow_html=True)
