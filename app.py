faab68ad7343aefb63b9c4297dcbce1ddcd44b2f
paulyoo97 <98494290+paulyoo97@users.noreply.github.com>
2024-01-24 22:48:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
 import streamlit as st
-from streamlit_option_menu import option_menu
-from PIL import Image
-
 st.set_page_config(page_title='In The Depths', page_icon='', layout='centered')    # layout by default is center.
 
 
